feeaf87cab9c688f8d9bfd9b730b23402b7b19a6
Jeremy McRae <jm33@sanger.ac.uk>
2016-10-13 12:57:19 +0100

    fix unit tests so that we hit required code paths. added test for w_part below zero

--- tests/test_conditional_approximation.py
+++ tests/test_conditional_approximation.py
@@ -3,6 +3,8 @@
 import unittest
 import random
 
+from math import isnan
+
 from numpy.random import beta, uniform, normal, seed
 
 from fitDNM.conditional_approximation import conditional_approximation
@@ -26,14 +28,26 @@ class TestConditionalApproximationPy(unittest.TestCase):
         y = 1
         lambdas = normal(loc=1e-4, scale=1e-5, size=1000)
         weights = beta(a=0.5, b=0.5, size=1000)
-        self.assertEqual(conditional_approximation(x, y, lambdas, weights), 0.01528782696)
+        self.assertEqual(conditional_approximation(x, y, lambdas, weights), 0.011558509232964654)
     
     def test_conditional_approximation_low_w_part(self):
         ''' check conditional_approximation for low w_part values
         '''
         
+        seed(13)
+        
         x = 2
         y = 1
-        lambdas = normal(loc=1e-5, scale=1e-5, size=1000)
+        lambdas = normal(loc=1e-4, scale=1e-5, size=1000)
         weights = uniform(size=1000)
-        self.assertEqual(conditional_approximation(x, y, lambdas, weights), 0.01528782696)
+        self.assertEqual(conditional_approximation(x, y, lambdas, weights), 0.49792912429037084)
+    
+    def test_conditional_approximation_w_part_below_zero(self):
+        ''' check conditional_approximation for w_part < 0
+        '''
+        
+        x = 1.001
+        y = 1
+        lambdas = normal(loc=1e-4, scale=1e-5, size=1000)
+        weights = uniform(size=1000)
+        self.assertTrue(isnan(conditional_approximation(x, y, lambdas, weights)))
