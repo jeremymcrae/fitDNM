a7630cbada5630a1c7809a06718f8511b2904e65
Jeremy McRae <jm33@sanger.ac.uk>
2016-10-13 16:31:45 +0100

    update unit test expected values for python random sampling

--- tests/test_double_saddlepoint_approximation.py
+++ tests/test_double_saddlepoint_approximation.py
@@ -2,6 +2,7 @@
 
 import unittest
 import random
+from math import isnan
 
 from numpy import array
 from numpy.random import beta, uniform, normal, seed
@@ -23,7 +24,7 @@ class TestDoubleSaddlePointPy(unittest.TestCase):
         y = 1
         lambdas = normal(size=1000, loc=1e-4, scale=1e-5)
         weights = beta(size=1000, a=0.5, b=0.5)
-        self.assertEqual(double_saddle_point_approximation(y, lambdas, weights), 0.002536151431)
+        self.assertEqual(double_saddle_point_approximation(y, lambdas, weights), 0.0022951333364715112)
     
     def test_double_saddle_point_approximation_zero_values(self):
         '''double_saddle_point_approximation output is correct when the start value equals 0
@@ -37,7 +38,7 @@ class TestDoubleSaddlePointPy(unittest.TestCase):
         
         y = 1
         weights = array([0] * 1000)
-        self.assertEqual(double_saddle_point_approximation(y, lambdas, weights), None)
+        self.assertTrue(isnan(double_saddle_point_approximation(y, lambdas, weights)))
     
     def test_double_saddle_point_approximation_uniform(self):
         '''double_saddle_point_approximation output is correct
@@ -46,4 +47,4 @@ class TestDoubleSaddlePointPy(unittest.TestCase):
         y = 1
         lambdas = normal(size=1000, loc=1e-5, scale=1e-5)
         weights = uniform(size=1000)
-        self.assertEqual(double_saddle_point_approximation(y, lambdas, weights), 2.477654823e-05)
+        self.assertEqual(double_saddle_point_approximation(y, lambdas, weights), 2.7451994001564722e-05)
