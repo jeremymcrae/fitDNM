611be8bf3afd2e19435b4023e79947c6dc32fc83
Jeremy McRae <jm33@sanger.ac.uk>
2016-10-26 16:32:03 +0100

    swapped to generating per base mutation rate table via denovonear

---
+++ fitDNM/mutation_rates.py
@@ -0,0 +1,77 @@
+
+
+import pandas
+
+from denovonear.load_gene import load_gene, \
+    get_de_novos_in_transcript
+from denovonear.ensembl_requester import EnsemblRequest
+from denovonear.load_mutation_rates import load_mutation_rates
+from denovonear.site_specific_rates import SiteRates
+
+def get_gene_rates(symbol, de_novos, ensembl=None, mut_dict=None):
+    ''' get per nucleotide mutation rates for all SNV alt alleles in a gene
+    
+    Args:
+        symbol: HGNC symbol for gene
+        de_novos: pandas DataFrame containing de novo candidates.
+        ensembl: EnsemblRequest object, for extracting gene coordinates and sequence.
+        mut_dict: list of lists for trinucleotide sequence context based
+            mutation rates.
+    
+    Returns:
+        pandas DataFrame of mutation rates at each possible SNV change within
+        the coding sequence of a gene.
+    '''
+    
+    if mut_dict is None:
+        mut_dict = load_mutation_rates()
+    
+    if ensembl is None:
+        ensembl = EnsemblRequest('cache', 'grch37')
+    
+    positions = de_novos['pos'][de_novos['gene'] == symbol]
+    transcripts = load_gene(ensembl, symbol, positions)
+    chrom = transcripts[0].get_chrom()
+    
+    # TODO: currently missing splice_lof sites, because they have been assigned
+    # TODO: to the nearest exon boundary, which duplicates positions and raises
+    # TODO: an error in the pivot_table()
+    mu_rate = []
+    for transcript in transcripts:
+        rates = SiteRates(transcript, mut_dict)
+        for cq in ['nonsense', 'missense', 'synonymous', 'splice_region']:
+            for choice in rates[cq]:
+                choice['pos'] = transcript.get_position_on_chrom(choice['pos'])
+                choice['gene'] = symbol
+                choice['chrom'] = chrom
+                mu_rate.append(choice)
+    
+    # convert the list of dictionaries to a DataFrame, then reshape to the
+    # required form for fitDNM
+    mu_rate = pandas.DataFrame(mu_rate)
+    mu_rate = mu_rate.pivot_table(rows=['gene', 'chrom', 'pos', 'ref'],
+        cols='alt', values='prob', fill_value=0.0)
+    
+    return flatten_indexed_table(mu_rate)
+
+def flatten_indexed_table(data):
+    ''' integrates MultiIndex columns in a DataFrame into the table
+    
+    Args:
+        data: pandas DataFrame, with a MultiIndex following a pivot_table()
+            reshaping of a DataFrame.
+    
+    Returns:
+        table where the index columns are now columns within the table
+    '''
+    
+    idx_names = list(data.index.names)
+    idx_data = [ list(data.index.get_level_values(x)) for x in idx_names ]
+    
+    col_names = list(data.columns)
+    col_data = [ list(data[x]) for x in col_names ]
+    
+    # convert to a dictionary, indexed by column names, to use in DataFrame
+    flattened = dict(zip(idx_names + col_names, idx_data + col_data))
+    
+    return pandas.DataFrame(flattened, columns=idx_names + col_names)
--- scripts/run_fitDNM.py
+++ scripts/run_fitDNM.py
@@ -1,19 +1,30 @@
 
+import os
 import argparse
 
 import pandas
 
 from fitDNM.prepare_data import get_rows_to_exclude, tidy_table
 from fitDNM.gene_enrichment import compute_pvalue
+from fitDNM.mutation_rates import get_gene_rates
 
 def get_options():
+    ''' parse the command line arguments
+    '''
+    
     parser = argparse.ArgumentParser()
     parser.add_argument('--males', type=int, help='number of males.')
     parser.add_argument('--females', type=int, help='number of females.')
     parser.add_argument('--de-novos', help='Path to table of de novos.')
-    parser.add_argument('--rates', help='Path to table of per-base and allele mutation rates.')
     parser.add_argument('--severity', help='Path to table of per base and allele severity scores.')
     parser.add_argument('--output', help='Path to put output files into.')
+    
+    parser.add_argument("--genome-build", dest="genome_build", choices=["grch37",
+        "GRCh37", "grch38", "GRCh38"], default="grch37", help="Genome build " \
+        "that the de novo coordinates are based on (GrCh37 or GRCh38")
+    parser.add_argument("--cache-folder", dest="cache_dir",
+        default=os.path.join(os.path.dirname(__file__), "cache"), help="folder" \
+        "to cache Ensembl data into (defaults to clustering code directory)")
     
     return parser.parse_args()
 
@@ -28,19 +39,23 @@ def main():
     # args.output = '../output.txt'
     
     de_novos = pandas.read_table(args.de_novos)
-    mu_rate = pandas.read_table(args.rates, compression='gzip')
+    
     severity = pandas.read_table(args.severity, compression='gzip')
-    
     exclude = get_rows_to_exclude(severity)
-    
-    mu_rate = tidy_table(mu_rate, exclude)
     severity = tidy_table(severity, exclude)
     
     computed = []
     for symbol in sorted(set(de_novos['gene'])):
         print(symbol)
+        
+        try:
+            mu_rate = get_gene_rates(symbol, de_novos)
+        except IndexError:
+            continue
+        
         values = compute_pvalue(de_novos, args.males, args.females, symbol,
                 severity, mu_rate)
+        
         if values is not None:
             computed.append(values)
         else:
