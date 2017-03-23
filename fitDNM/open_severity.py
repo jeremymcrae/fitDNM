a0897fa54d742c31cfa9f71ec6674153ee142055
Jeremy McRae <jm33@sanger.ac.uk>
2017-03-23 13:39:32 +0000

    change how we load severity scores

--- fitDNM/open_severity.py
+++ fitDNM/open_severity.py
@@ -2,8 +2,6 @@
 import pandas
 import pysam
 
-from fitDNM.mutation_rates import flatten_indexed_table
-
 def get_cadd_severity(symbol, chrom, start, end,
         cadd_path='/lustre/scratch113/projects/ddd/users/ps14/CADD/whole_genome_SNVs.tsv.gz'):
     ''' get per nucleotide mutation rates for all SNV alt alleles in a gene
@@ -29,15 +27,13 @@ def get_cadd_severity(symbol, chrom, start, end,
         chrom, pos, ref, alt, raw, scaled = line.split('\t')
         return {'chrom': chrom, 'pos': int(pos), 'ref': ref, 'alt': alt,
             'raw': float(raw), 'score': float(scaled)}
-        
+    
     cadd = pandas.DataFrame([ parse(x) for x in tabix.fetch(chrom, start, end) ])
     
-    # scale the cadd scores between 0-1
-    cadd['score'] = 1 - 10**-(cadd['score']/10)
+    cadd = cadd.sort_values(['chrom', 'pos', 'alt'])
+    cadd = cadd.reset_index(drop=True)
     cadd['gene'] = symbol
     cadd['chrom'] = cadd['chrom'].astype(str)
+    del cadd['raw']
     
-    cadd = cadd.pivot_table(index=['chrom', 'pos', 'gene', 'ref'],
-        columns='alt', values='score' ,fill_value=0.0)
-    
-    return flatten_indexed_table(cadd)
+    return cadd
--- tests/test_open_severity.py
+++ tests/test_open_severity.py
@@ -28,12 +28,12 @@ class TestOpenSeverityPy(CompareTables):
         cadd_path = os.path.join(os.path.dirname(__file__), 'data', 'cadd.txt.gz')
         
         severity = get_cadd_severity(symbol, chrom, start, end, cadd_path)
-        tab = StringIO('''gene chrom pos      ref            A           C           G           T
-                         OR5A1  11   59210642   A   0.00000000  0.86871956  0.53516373  0.87390438
-                         OR5A1  11   59210643   T   0.89641425  0.10339738  0.84070578  0.00000000
-                         OR5A1  11   59210644   G   0.89287273  0.84281901  0.00000000  0.86401218
-                         OR5A1  11   59210645   T   0.00069053  0.00275928  0.00023023  0.00000000
-                         OR5A1  11   59210646   C   0.08924828  0.00000000  0.82461194  0.35597904''')
+        tab = StringIO('''gene chrom pos      ref  alt   score
+                         OR5A1  11   59210642   A    C   8.818
+                         OR5A1  11   59210642   A    G   3.327
+                         OR5A1  11   59210642   A    T   8.993
+                         OR5A1  11   59210643   T    A   9.847
+                         OR5A1  11   59210643   T    C   0.474''')
         
         data = pandas.read_table(tab, delim_whitespace=True, skipinitialspace=True)
         data = data.reset_index(drop=True)
