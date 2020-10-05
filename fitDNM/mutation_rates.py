8999c2f0911a9c766aa16f35106b450186b1d527
Jeremy McRae <jmcrae@illumina.com>
2020-10-05 14:46:40 -0700

    updated loading genes from ensembl via denovonear async calls

--- fitDNM/mutation_rates.py
+++ fitDNM/mutation_rates.py
@@ -1,4 +1,5 @@
 
+import asyncio
 
 import pandas
 
@@ -6,17 +7,33 @@ from fitDNM.regional_constraint import load_regional_constraint, get_constrained
 
 from denovonear.load_gene import load_gene, \
     get_de_novos_in_transcript, construct_gene_object
-from denovonear.ensembl_requester import EnsemblRequest
 from denovonear.load_mutation_rates import load_mutation_rates
 from denovonear.site_specific_rates import SiteRates
+from denovonear.rate_limiter import RateLimiter
 
-def get_gene_rates(symbol, de_novos, constraint=None, ensembl=None, mut_path=None):
+def get_gene_rates(symbol, de_novos, constraint=None, mut_path=None):
+    ''' get per nucleotide mutation rates for all SNV alt alleles in a gene
+
+    Args:
+        symbol: HGNC symbol for gene
+        de_novos: pandas DataFrame containing de novo candidates.
+        mut_path: path to table of sequence context based mutation rates.
+
+    Returns:
+        pandas DataFrame of mutation rates at each possible SNV change within
+        the coding sequence of a gene.
+    '''
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(_async_get_gene_rates(
+        symbol, de_novos, constraint, mut_path))
+    return result
+
+async def _async_get_gene_rates(symbol, de_novos, constraint=None, mut_path=None):
     ''' get per nucleotide mutation rates for all SNV alt alleles in a gene
     
     Args:
         symbol: HGNC symbol for gene
         de_novos: pandas DataFrame containing de novo candidates.
-        ensembl: EnsemblRequest object, for extracting gene coordinates and sequence.
         mut_path: path to table of sequence context based mutation rates.
     
     Returns:
@@ -26,25 +43,23 @@ def get_gene_rates(symbol, de_novos, constraint=None, ensembl=None, mut_path=Non
     
     mut_dict = load_mutation_rates(mut_path)
     
-    if ensembl is None:
-        ensembl = EnsemblRequest('cache', 'grch37')
-    
     if constraint is not None:
         constraint = load_regional_constraint(constraint)
     else:
         constraint = {'gene': set([])}
     
-    positions = de_novos['pos'][de_novos['gene'] == symbol]
-    transcripts = load_gene(ensembl, symbol, positions)
-    chrom = transcripts[0].get_chrom()
-    
-    if symbol not in set(constraint['gene']):
-        sites = set([])
-    else:
-        regional = constraint[constraint['gene'] == symbol]
-        tx_id = list(regional['transcript'])[0]
-        tx = construct_gene_object(ensembl, tx_id.split('.')[0])
-        sites = get_constrained_positions(tx, regional, threshold=1e-3, ratio_threshold=0.4)
+    async with RateLimiter(per_second=15) as ensembl:
+        positions = de_novos['pos'][de_novos['gene'] == symbol]
+        transcripts = await load_gene(ensembl, symbol, positions)
+        chrom = transcripts[0].get_chrom()
+        
+        if symbol not in set(constraint['gene']):
+            sites = set([])
+        else:
+            regional = constraint[constraint['gene'] == symbol]
+            tx_id = list(regional['transcript'])[0]
+            tx = await construct_gene_object(ensembl, tx_id.split('.')[0])
+            sites = get_constrained_positions(tx, regional, threshold=1e-3, ratio_threshold=0.4)
     
     mu_rate = []
     for transcript in transcripts:
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name = "fitDNM",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Yu Jiang",
     author_email = "yu.jiang@duke.edu",
     description = ("Enrichment of de novo mutations within genes."),
